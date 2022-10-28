7d3993b1032942e34baa8aff4ab08e34172e8b92
dadams2001 <dadams22@nd.edu>
2022-10-28 15:24:59 -0400

    App now fetches data from backend, can search by item subcategory

--- src/Components/ItemCard/ItemCard.js
+++ src/Components/ItemCard/ItemCard.js
@@ -6,12 +6,10 @@ const ItemCard = ({merch, index}) => {
         <li className="merchCard" key={index}>
             <img id="ashley" src={ashley} alt="idk"/> 
             <div className='merchCard-subcontainer'>
-                <h3>{merch.item_name}</h3>
-                <p>
-                    {merch.size}
-                    <br/>
-                    {merch.price}
-                </p>
+                <div className='subcontainer-row'><p>{merch[0]} {merch[4]} {merch[7]}</p></div>
+                <div className='subcontainer-row'><p>{merch[1]} ${merch[3]} {merch[6]} ({merch[2]})</p></div>
+                <div className='subcontainer-row'><p>Quantity: {merch[8]} Box: {merch[9]} Shelf: {merch[10]} Location: {merch[11]}</p></div>
+                <div className='subcontainer-row'><button>Record Sale</button><button>Restock</button></div>
             </div>
         </li>
     );
